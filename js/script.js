5f6c3d606224760b0d5e66089c441fa9eb0ab375
Sameer Hussain <87130217+Sameer0018@users.noreply.github.com>
2022-12-27 11:36:32 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ============================== typing animation ============================ */
 var typed = new Typed(".typing",{
-    strings:["","Web Developer","Programmer","CS Student"],
+    strings:["","Web Developer","Programmer","IT Student"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
